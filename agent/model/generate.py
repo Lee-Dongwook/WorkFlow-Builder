24bbd6a8a0bdcbc456e0ded04f745cd56d9aa9d9
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-12-17 16:14:20 +0900

    feat: top-p (Nucleus) Sampling

--- agent/model/generate.py
+++ agent/model/generate.py
@@ -20,16 +20,38 @@ def sample(logits, temperature=1.0, top_k=10):
     idx = torch.multinomial(probs, 1)
     return indices[idx]
 
-def generate(start, length=100, temperature=0.8, top_k=5):
-    ids = tokenizer.encode(start)
+@torch.no_grad()
+def generate(model, tokenizer, prompt, max_new_tokens = 50, temperature=1.0, top_p=0.9):
+    model.eval()
 
-    for _ in range(length):
-        x = torch.tensor([ids[-block_size:]])
-        logits = model(x)
-        next_id = sample(logits[0, -1], temperature, top_k)
-        ids.append(next_id.item())
+    idx = torch.tensor(tokenizer.encode(prompt), dtype=torch.long).unsqueeze(0)
 
-    return tokenizer.decode(ids)
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -model.block_size:]
+        logits = model(idx_cond)
+
+        logits = logits[:, -1, :] / temperature
+        probs = F.softmax(logits, dim=-1)
+
+        sorted_probs, sorted_idx = torch.sort(
+            probs, descending=True
+        )
+        
+        cumulative_probs = torch.cumsum(sorted_probs, dim=-1)
+
+        cutoff = cumulative_probs > top_p
+        cutoff[..., 1:] = cutoff[..., :-1].clone()
+        cutoff[..., 0] = False
+
+        sorted_probs[cutoff] = 0.0
+        sorted_probs /= sorted_probs.sum(dim=-1, keepdim=True) 
+
+        next_token = torch.multinomial(sorted_probs, num_samples=1)
+        next_idx = torch.gather(sorted_idx, -1, next_token)
+
+        idx = torch.cat([idx, next_idx], dim=1)
+
+    return tokenizer.decode(idx[0].tolist())
 
 prompt = """### Instruction:
 사과 바나나 포도를 순서대로 처리해라
@@ -37,4 +59,4 @@ prompt = """### Instruction:
 ### Response:
 """
 
-print(generate(prompt, temperature=0.7, top_k=5))
+print(generate(model, tokenizer, "### Instruction:\n순서를 지켜서 사과 바나나 포도 실행\n\n### Response:\n", temperature=0.8, top_p=0.9))
