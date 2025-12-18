dfa6c4de4ddd5024c96d7b4a97cab3d3552663d4
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-12-18 14:17:38 +0900

    feat: add Speical token on BPE

--- agent/model/tokenizer/bpe_class.py
+++ agent/model/tokenizer/bpe_class.py
@@ -3,25 +3,59 @@ import os
 from .bpe import train_bpe, build_vocab, bpe_encode, decode
 
 class BPETokenizer:
+    SPECIAL_TOKENS = {
+        "<|bos|>": 0,
+        "<|eos|>": 1,
+        "<|pad|>": 2,
+    }
+
     def __init__(self, text=None, num_merges=500):
         if text is not None:
             self.merges = train_bpe(text, num_merges)
-            self.stoi, self.itos = build_vocab(self.merges)
+            base_stoi, base_itos = build_vocab(self.merges)
+
+            self.stoi = {**self.SPECIAL_TOKENS}
+            self.itos = {v: k for k, v in self.SPECIAL_TOKENS.items()}
+
+            offset = len(self.SPECIAL_TOKENS)
+            for token, idx in base_stoi.items():
+                self.stoi[token] = idx + offset
+                self.itos[idx + offset] = token
         else:
             self.merges = []
-            self.stoi = {}
-            self.itos = {}
+            self.stoi = {**self.SPECIAL_TOKENS}
+            self.itos = {v:k for k, v in self.SPECIAL_TOKENS.items()}
 
     @property
     def vocab_size(self):
         return len(self.stoi)
 
-    def encode(self, text):
-        tokens = bpe_encode(text, self.merges)
-        return [self.stoi[t] for t in tokens if t in self.stoi]
+    @property
+    def bos_id(self):
+        return self.SPECIAL_TOKENS["<|bos|>"]
 
-    def decode(self, ids):
-        tokens = [self.itos[i] for i in ids]
+    @property
+    def eos_id(self):
+        return self.SPECIAL_TOKENS["<|eos|>"]
+
+    @property
+    def pad_id(self):
+        return self.SPECIAL_TOKENS["<|pad|>"]
+
+    def encode(self, text, add_bos=False, add_eos=False):
+        tokens = bpe_encode(text, self.merges)
+        ids =  [self.stoi[t] for t in tokens if t in self.stoi]
+        if add_bos:
+            ids = [self.bos_id] + ids
+        if add_eos:
+            ids = ids + [self.eos_id]
+        return ids
+        
+
+    def decode(self, ids, skip_special=True):
+        if skip_special:
+            ids = [i for i in ids if i not in self.SPECIAL_TOKENS.values()]
+        tokens = [self.itos[i] for i in ids if i in self.itos]
         return decode(tokens)
 
     def save(self, path):
@@ -38,5 +72,6 @@ class BPETokenizer:
             tokenizer.merges = [tuple(m) for m in json.load(f)]
         with open(os.path.join(path, "vocab.json"), "r") as f:
             tokenizer.stoi = json.load(f)
+            
         tokenizer.itos = {int(i): s for s, i in tokenizer.stoi.items()}
         return tokenizer
